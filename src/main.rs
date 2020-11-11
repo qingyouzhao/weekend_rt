5da179777e1b2ec8174995c128865fed135dcddf
zqy <zhaoqingyou315@gmail.com>
2020-11-10 17:18:34 -0800

    Render a simple image as starter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate image;
+
+use image::{GenericImage, GenericImageView, ImageBuffer, RgbImage}; 
+
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+  let image_width:u32 = 256;
+  let image_height:u32 = 256;
+
+  let mut img: RgbImage = ImageBuffer::new(image_width, image_height);
+
+  for j in 0..image_height{
+    for i in 0..image_width{
+      let r = (i as f32) / (image_width -1) as f32;
+      let g = (j as f32) / (image_height -1) as f32;
+      let b = 0.25;
+
+      let ir:u8 = (255.99 * r) as u8;
+      let ig:u8 = (255.99 * g) as u8;
+      let ib:u8 = (255.99 * b) as u8;
+      img.put_pixel(i,j, image::Rgb::<u8>([ir, ig, ib]));
+    }
+  }
+  img.save("test.png").unwrap();
 }
